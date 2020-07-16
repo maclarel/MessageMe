8eae7138be8e78a3acdbbf5f8e07da842677fdf2
Logan MacLaren <maclarel@github.com>
2020-07-16 10:11:01 -0400

    login handling and flash messages using semantic and javascript

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,23 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    user = User.find_by(username: params[:session][:username])
+
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Log in successful!"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Invalid username or password."
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Logged out successfully."
+    redirect_to root_path
+  end
+
 end
